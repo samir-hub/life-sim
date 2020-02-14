baca87bad2f33f8422508f6ca632be948ed8f0a4
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-14 18:00:03 -0500

    added navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import EntryForm from "./components/EntryForm";
+import Home from './components/Home';
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
+      <Route path="/" component={Home} />
       <Route path="/entryform" component={EntryForm} />
       <Route path="/dashboard" component={Dashboard} />
     </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <NavLink to='/entryform'>Get Started</NavLink>
+      <NavLink to='/dashboard'>My Dashboard</NavLink>
+    </div>
+  );
+}
+
+export default Navigation;
