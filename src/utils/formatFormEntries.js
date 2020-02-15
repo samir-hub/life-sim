02fe876f1ff06e75cc3528aaf3d79aa60ee3ba40
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-14 23:04:28 -0500

    fixed formatFormEntries function

--- src/utils/formatFormEntries.js
+++ src/utils/formatFormEntries.js
@@ -17,79 +17,79 @@ export function formatFormEntries(formEntry) {
   }
   switch (formEntry.major) {
     case "Arts and Humanities":
-      formEntry.education = 0;
+      formEntry.major = 0;
       break;
     case "Business":
-      formEntry.education = 1;
+      formEntry.major = 1;
       break;
     case "Education":
-      formEntry.education = 2;
+      formEntry.major = 2;
       break;
     case "Music":
-      formEntry.education = 3;
+      formEntry.major = 3;
       break;
     case "Engineering":
-      formEntry.education = 4;
+      formEntry.major = 4;
       break;
     case "Nursing":
-      formEntry.education = 5;
+      formEntry.major = 5;
       break;
     case "Medicine":
-      formEntry.education = 6;
+      formEntry.major = 6;
       break;
     case "Social Sciences":
-      formEntry.education = 7;
+      formEntry.major = 7;
       break;
     case "Hard Sciences":
-      formEntry.education = 8;
+      formEntry.major = 8;
       break;
     default:
       return formEntry;
   }
   switch (formEntry.state) {
     case "No State Income Tax":
-      formEntry.education = 0;
+      formEntry.state = 0;
       break;
     case "State Income Tax":
-      formEntry.education = 1;
+      formEntry.state = 1;
       break;
     default:
       return formEntry;
   }
   switch (formEntry.city) {
     case "Small City":
-      formEntry.education = 0;
+      formEntry.city = 0;
       break;
     case "Medium City":
-      formEntry.education = 1;
+      formEntry.city = 1;
       break;
     case "Large City":
-      formEntry.education = 2;
+      formEntry.city = 2;
       break;
     case "Very Large City":
-      formEntry.education = 3;
+      formEntry.city = 3;
       break;
     case "Immense City":
-      formEntry.education = 2;
+      formEntry.city = 2;
       break;
     case "NYC/SF/Honolulu/Seattle/DC/Oakland/Boston/LA":
-      formEntry.education = 3;
+      formEntry.city = 3;
       break;
     default:
       return formEntry;
   }
   switch (formEntry.col) {
     case "Low Cost of Living":
-      formEntry.education = 0;
+      formEntry.col = 0;
       break;
     case "Medium Cost of Living":
-      formEntry.education = 1;
+      formEntry.col = 1;
       break;
     case "High Cost of Living":
-      formEntry.education = 2;
+      formEntry.col = 2;
       break;
-    case "Very High Cost of Living":
-      formEntry.education = 3;
+    case "Very Cost of Living":
+      formEntry.col = 3;
       break;
     default:
       return formEntry;
