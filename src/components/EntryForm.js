39e3d279e386bf6439696774b28883f9fd9d8b2e
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-14 19:26:48 -0500

    added more DD menu options

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -31,8 +31,12 @@ function EntryForm() {
       <h1>Enter Your Info: </h1>
 
       <form onSubmit={handlePostEntry} className="userentry-form">
-
-        <select name="education" onChange={handleChange} value={userEntry.education}>
+        <select
+          name="education"
+          onChange={handleChange}
+          value={userEntry.education}
+        >
+          <option>Select Your Education Level</option>
           <option>No College</option>
           <option>Community College</option>
           <option>In-State College</option>
@@ -40,30 +44,39 @@ function EntryForm() {
         </select>
 
         <select name="major" onChange={handleChange} value={userEntry.major}>
-          <option>No College</option>
-          <option>Community College</option>
-          <option>In-State College</option>
-          <option>Out-of-State College</option>
+          <option>Select Your Major</option>
+          <option>Arts and Humanities</option>
+          <option>Business</option>
+          <option>Education</option>
+          <option>Music</option>
+          <option>Engineering</option>
+          <option>Nursing</option>
+          <option>Medicine</option>
+          <option>Social Sciences</option>
+          <option>Hard Sciences</option>
         </select>
 
         <select name="state" onChange={handleChange} value={userEntry.state}>
-          <option>No College</option>
-          <option>Community College</option>
-          <option>In-State College</option>
-          <option>Out-of-State College</option>
+          <option>State Income Tax?</option>
+          <option>No State Income Tax</option>
+          <option>State Income Tax</option>
         </select>
         <select name="city" onChange={handleChange} value={userEntry.city}>
-          <option>No College</option>
-          <option>Community College</option>
-          <option>In-State College</option>
-          <option>Out-of-State College</option>
+          <option>City Size</option>
+          <option>Small City</option>
+          <option>Medium City</option>
+          <option>Large City</option>
+          <option>Very Large City</option>
+          <option>Immense City</option>
+          <option>NYC/SF/Honolulu/Seattle/DC/Oakland/Boston/LA</option>
         </select>
 
         <select name="col" onChange={handleChange} value={userEntry.col}>
-          <option>No College</option>
-          <option>Community College</option>
-          <option>In-State College</option>
-          <option>Out-of-State College</option>
+          <option>Cost of Living</option>
+          <option>Low Cost of Living</option>
+          <option>Medium Cost of Living</option>
+          <option>High Cost of Living</option>
+          <option>Very High Cost of Living</option>
         </select>
 
         <button>Submit</button>
