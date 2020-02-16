7fbb846e43b9e91635a68e7a500b327257c529ab
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-15 23:23:43 -0500

    added home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import guy_crossing_arms from '../guy_crossing_arms.png';
 
 function Home() {
   return (
-    <div className="home">
-      
+    <div style={{display: 'flex', justifyContent:'flex-end'}} className="home">
+      <img style={{height: '90vh'}} alt='Guy crossing arms' src={guy_crossing_arms}></img>
     </div>
   );
 }
