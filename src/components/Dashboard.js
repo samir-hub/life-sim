0e13bc248578ffc84975eb7090dff5fce68c53e4
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-17 23:49:43 -0500

    added card to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from "react";
+import Icon from "antd/es/icon";
+import "antd/es/icon/style/css";
+import Avatar from "antd/es/avatar";
+import "antd/es/avatar/style/css";
+import Card from "antd/es/card";
+import "antd/es/card/style/css";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchEntry } from "../actions";
 import { entriesIntToString } from "../utils/entriesIntToString";
+
+const { Meta } = Card;
 
 function Dashboard() {
   const state = useSelector(state => {
@@ -23,6 +31,28 @@ function Dashboard() {
       <header className="dashboard-header">
         <h1>Dashboard</h1>
       </header>
+      <Card
+        style={{ width: 300 }}
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={[
+          <Icon type="setting" key="setting" />,
+          <Icon type="edit" key="edit" />,
+          <Icon type="ellipsis" key="ellipsis" />
+        ]}
+      >
+        <Meta
+          avatar={
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          }
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
 
       <div key={string.user}>
         <p>Education Code: {string.education}</p>
