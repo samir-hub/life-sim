4949a06dd56389f5f083c1ed2ff7244e1f18b52b
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-03-22 21:30:39 -0400

    WIP: styling mobile income

--- src/components/Income.js
+++ src/components/Income.js
@@ -32,14 +32,20 @@ function Income() {
       <Paragraph
         style={{
           textAlign: "left",
-          fontSize: '15px'
+          fontSize: "15px"
         }}
       >
         Many different factors will determine your income. We use your
         information to calculate an estimate that will help you plan for the
         future. Many different factors will determine your income. We use your
         information to calculate an estimate that will help you plan for the
-        future. <strong>Gross Monthly Income</strong> refers to the estimated monthly income <strong>before</strong> any taxes or any other deductions are taken out. <strong>Net Monthly Income</strong> refers to the estimated monthly income <strong>after</strong> all taxes and other deductions are taken out. A <strong>Biweekly Pay Stub</strong> is a detailed view of your income and deductions for a two week period. Most people get paid every two weeks. 
+        future. <strong>Gross Monthly Income</strong> refers to the estimated
+        monthly income <strong>before</strong> any taxes or any other deductions
+        are taken out. <strong>Net Monthly Income</strong> refers to the
+        estimated monthly income <strong>after</strong> all taxes and other
+        deductions are taken out. A <strong>Biweekly Pay Stub</strong> is a
+        detailed view of your income and deductions for a two week period. Most
+        people get paid every two weeks.
       </Paragraph>
     </div>
   );
@@ -65,165 +71,200 @@ function Income() {
 
   const columns = [
     {
-      title: 'Earnings',
-      dataIndex: 'earnings',
-      key: 'earnings',
+      title: "Earnings",
+      dataIndex: "earnings",
+      key: "earnings"
     },
     {
-      title: 'rate',
-      dataIndex: 'rate',
-      key: 'rate',
+      title: "rate",
+      dataIndex: "rate",
+      key: "rate"
     },
     {
-      title: 'hours',
-      dataIndex: 'hours',
-      key: 'hours',
+      title: "hours",
+      dataIndex: "hours",
+      key: "hours"
     },
     {
-      title: 'total',
-      dataIndex: 'total',
-      key: 'total',
-    },
+      title: "total",
+      dataIndex: "total",
+      key: "total"
+    }
   ];
-  
+
   const data = [
     {
-      key: '1',
-      earnings: 'Regular',
-      rate: (state.userInfo.details[state.userInfo.details.length - 1]
-      .avgmajor / 1920).toFixed(2),
+      key: "1",
+      earnings: "Regular",
+      rate: (
+        state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+        1920
+      ).toFixed(2),
       hours: 80,
-      total: ((state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-
+      total: (
+        state.userInfo.details[state.userInfo.details.length - 1].avgmajor / 24
+      )
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     },
     {
-      key: '2',
-      earnings: 'PTO',
-      rate: (state.userInfo.details[state.userInfo.details.length - 1]
-      .avgmajor / 1920).toFixed(2),
+      key: "2",
+      earnings: "PTO",
+      rate: (
+        state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+        1920
+      ).toFixed(2),
       hours: 0,
       total: 0
-    
     },
     {
-      key: '3',
-      earnings: 'Overtime',
-      rate: (state.userInfo.details[state.userInfo.details.length - 1]
-      .avgmajor / 1920 * 1.5).toFixed(2),
+      key: "3",
+      earnings: "Overtime",
+      rate: (
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          1920) *
+        1.5
+      ).toFixed(2),
       hours: 0,
       total: 0
-
     },
     {
-      key: '4',
-      earnings: '',
-      rate: 'Gross Pay',
-      hours: '',
-      total: ((state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-
+      key: "4",
+      earnings: "",
+      rate: "Gross Pay",
+      hours: "",
+      total: (
+        state.userInfo.details[state.userInfo.details.length - 1].avgmajor / 24
+      )
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     }
   ];
 
   const deduColumns = [
     {
-      title: 'Deductions',
-      dataIndex: 'deductions',
-      key: 'deductions',
+      title: "Deductions",
+      dataIndex: "deductions",
+      key: "deductions"
     },
     {
-      title: 'Statutory',
-      dataIndex: 'statutory',
-      key: 'statutory',
+      title: "Statutory",
+      dataIndex: "statutory",
+      key: "statutory"
     },
     {
-      title: '',
-      dataIndex: 'hours',
-      key: 'hours',
+      title: "",
+      dataIndex: "hours",
+      key: "hours"
     },
     {
-      title: '',
-      dataIndex: 'total',
-      key: 'total',
-    },
+      title: "",
+      dataIndex: "total",
+      key: "total"
+    }
   ];
-  
+
   const deduData = [
     {
-      key: '1',
-      statutory: 'Federal Income Tax',
-      hours: '',
-      total: -(state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24 * 0.1).toFixed(2)
-
+      key: "1",
+      statutory: "Federal Income Tax",
+      hours: "",
+      total: -(
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+        0.1
+      ).toFixed(2)
     },
     {
-      key: '2',
-      statutory: 'Social Security Tax',
-      hours: '',
-      total: -(state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24 * 0.062).toFixed(2)
-    
+      key: "2",
+      statutory: "Social Security Tax",
+      hours: "",
+      total: -(
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+        0.062
+      ).toFixed(2)
     },
     {
-      key: '3',
-      statutory: 'Medicare Tax',
-      hours: '',
-      total: -(state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24 * 0.009).toFixed(2)
-
+      key: "3",
+      statutory: "Medicare Tax",
+      hours: "",
+      total: -(
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+        0.009
+      ).toFixed(2)
     },
     {
-      key: '4',
-      statutory: 'Net Pay',
-      hours: '',
-      total: (((state.userInfo.details[state.userInfo.details.length - 1]
-        .avgmajor / 24) - (state.userInfo.details[state.userInfo.details.length - 1]
-          .avgmajor / 24 * 0.1) - (state.userInfo.details[state.userInfo.details.length - 1]
-            .avgmajor / 24 * 0.062) - (state.userInfo.details[state.userInfo.details.length - 1]
-              .avgmajor / 24 * 0.009)).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-
+      key: "4",
+      statutory: "Net Pay",
+      hours: "",
+      total: (
+        state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24 -
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+          0.1 -
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+          0.062 -
+        (state.userInfo.details[state.userInfo.details.length - 1].avgmajor /
+          24) *
+          0.009
+      )
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     }
   ];
 
   return (
     <StyledDiv>
       <PageHeader
-        title={
-          <img
-            className="income-icon"
-            alt="wallet"
-            src={wallet}
-          />
-        }
+        title={<img className="income-icon" alt="wallet" src={wallet} />}
         className="site-page-header"
         extra={[
           <React.Fragment key="1">
-            <h3 key="1" style={{ margin: "0px", width: "100%" }}>
+            <h3
+              className="income-h3"
+              key="1"
+              style={{ margin: "0px", width: "100%" }}
+            >
               {` Est. Gross Monthly Income: $`}
               {state.isFetching ? (
                 <p>fetching</p>
               ) : (
                 state.userInfo.details[state.userInfo.details.length - 1] &&
-                ((
+                (
                   state.userInfo.details[state.userInfo.details.length - 1]
                     .avgmajor / 12
-                ).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                )
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
               )}
             </h3>
-            <h3 key="2" style={{ margin: "0px", width: "100%" }}>
+            <h3
+              className="income-h3"
+              key="2"
+              style={{ margin: "0px", width: "100%" }}
+            >
               {` Est. Net Monthly Income: $`}
               {state.isFetching ? (
                 <p>fetching</p>
               ) : (
                 state.userInfo.details[state.userInfo.details.length - 1] &&
-                ((
+                (
                   (state.userInfo.details[state.userInfo.details.length - 1]
                     .avgmajor /
                     12) *
                   0.85
-                ).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                )
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
               )}
             </h3>
           </React.Fragment>
@@ -233,8 +274,14 @@ function Income() {
       </PageHeader>
       <Card className="income-card" hoverable={true}>
         <h1>Biweekly Pay Stub:</h1>
-        <Table pagination={false} columns={columns} dataSource={data} />
-        <Table pagination={false} columns={deduColumns} dataSource={deduData} />
+        <div className="income-tables">
+          <Table pagination={false} columns={columns} dataSource={data} />
+          <Table
+            pagination={false}
+            columns={deduColumns}
+            dataSource={deduData}
+          />
+        </div>
       </Card>
     </StyledDiv>
   );
@@ -246,25 +293,54 @@ const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  .income-icon {
-    height: 50px; 
-    margin-right: 10px; 
+  .income-h3 {
+    margin: 0;
+    width: 100%;
     @media only screen and (max-width: 600px) {
-      height: 40px;  
+      width: 60%;
+      font-size: 12px;
+      text-align: right;
+    }
+  }
+  .income-icon {
+    height: 50px;
+    margin-right: 10px;
+    @media only screen and (max-width: 600px) {
+      height: 40px;
     }
   }
   .ant-page-header {
     margin-top: 13px;
     border: 1px solid #e8e8e8;
   }
+  .ant-page-header-heading {
+    @media only screen and (max-width: 600px) {
+      display: flex;
+      align-content: center;
+    }
+  }
+  .ant-page-header-content {
+    @media only screen and (max-width: 600px) {
+      height: 100px;
+      overflow-y: scroll;
+    }
+  }
+  .ant-page-header-heading-extra {
+    @media only screen and (max-width: 600px) {
+      padding: 0;
+    }
+  }
   .income-card {
     margin-top: 10px;
     margin-bottom: 10px;
     height: 85vh;
     width: 45%;
+    @media only screen and (max-width: 600px) {
+      width: 95%;
+    }
     .ant-table-row:nth-child(4) {
-    background: lightgray;
-  }
+      background: lightgray;
+    }
     .income-earnings {
       display: flex;
       align-items: flex-end;
@@ -279,7 +355,7 @@ const StyledDiv = styled.div`
         width: 40%;
         margin-right: 100px;
         margin-bottom: 0;
-        text-align: left; 
+        text-align: left;
       }
       .income-earnings-subtitle {
         padding: 0 10px;
