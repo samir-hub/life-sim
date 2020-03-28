4b23521954c1f90fbedc08d9dc80d4e6a77bf99a
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-03-27 22:45:46 -0400

    changed from localhost to heroku

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -7,7 +7,7 @@ const axiosWithAuth = () => {
     headers: {
       Authorization: `Bearer ${token}`
     },
-    baseURL: `http://localhost:2019`
+    baseURL: `https://samirlilienfeld-mypath.herokuapp.com`
   });
 };
 
