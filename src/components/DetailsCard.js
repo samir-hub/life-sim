eb37d676a93238646960d832d3311931d82d2167
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-04-15 19:34:37 -0400

    styling details cards

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -3,6 +3,8 @@ import Card from "antd/es/card";
 import "antd/es/card/style/css";
 import Avatar from "antd/es/avatar";
 import "antd/es/avatar/style/css";
+import Button from "antd/es/button";
+import "antd/es/button/style/css";
 import styled from "styled-components";
 
 const { Meta } = Card;
@@ -12,9 +14,16 @@ function DetailsCard({ education, major, city, avatar }) {
     <StyledDiv>
       <Card className="card">
         <Meta avatar={<Avatar src={avatar} />} />
-        <p style={{marginTop: "10px"}}>{education}</p>
-        <p>{major}</p>
-        <p>{city}</p>
+        <div className="dc-card-content">
+          <div className="dc-card-text">
+            <h1 className="dc-text" style={{ marginTop: "10px" }}>
+              {education}
+            </h1>
+            <h1 className="dc-text">{major}</h1>
+            <h1 className="dc-text">{city}</h1>
+          </div>
+          <Button>Compare</Button>
+        </div>
       </Card>
     </StyledDiv>
   );
@@ -26,5 +35,16 @@ const StyledDiv = styled.div`
   width: 40%;
   .card {
     margin: 20px 0;
+    .dc-card-content {
+      display: flex;
+      align-items: center; 
+      .dc-card-text {
+        width: 80%;
+        .dc-text {
+          text-align: left;
+          width: 80%;
+        }
+      }
+    }
   }
 `;
